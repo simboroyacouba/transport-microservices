78c8aca23c9d95a4edf3c78704a4624b6787eb52
Yacouba Simboro <yacouba.simboro@nebrata.com>
2025-05-30 11:32:50 +0000

    Add swagger  et correctionserreurs cors

--- paiement-service/src/main/java/com/ibam/paiementservice/repository/ReceipRepository.java
+++ paiement-service/src/main/java/com/ibam/paiementservice/repository/ReceipRepository.java
@@ -2,9 +2,9 @@ package com.ibam.paiementservice.repository;
 
 import com.ibam.paiementservice.entities.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 
-@RepositoryRestResource
+@Repository
 public interface ReceipRepository extends JpaRepository<Receipt, Long> {
 }
