89490b0f90b4302a9d5fa55998c7cf876930d9dd
serge-eric-kalaga <kalagaserge4@gmail.com>
2025-06-02 09:46:33 +0000

    Ajout des relations entre les modèles Vehicle, Driver et Trip

--- fleet-service/app/models/Models.js
+++ fleet-service/app/models/Models.js
@@ -4,6 +4,13 @@ const { Trip, CreateTripModel, UpdateTripModel } = require("./Trip.model");
 const { DB, connect_db } = require("../configs/Database");
 
 
+// Relation entre les modèles
+Vehicle.hasMany(Trip, { foreignKey: "vehicleId" });
+Trip.belongsTo(Vehicle, { foreignKey: "vehicleId" });
+Driver.hasMany(Trip, { foreignKey: "driverId" });
+Trip.belongsTo(Driver, { foreignKey: "driverId" });
+
+
 module.exports = {
   // --- Vehicles 
   Vehicle,
